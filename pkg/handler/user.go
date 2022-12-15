f1bd9f28193188418ec21ad2514341e61bd00741
globalskye <43073544+globalskye@users.noreply.github.com>
2022-12-15 03:43:22 +0300

    feat: env variables and returns to the handlers

--- pkg/handler/user.go
+++ pkg/handler/user.go
@@ -18,7 +18,9 @@ func (h *Handler) GetOnline(c *gin.Context) {
 	users, err := h.services.UserI.GetOnline()
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 	c.JSON(http.StatusOK, gin.H{"users": users})
+	return
 
 }
--- pkg/repository/mongoDB.go
+++ pkg/repository/mongoDB.go
@@ -5,11 +5,12 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"os"
 )
 
 func NewMongoConnect() (*mongo.Database, error) {
 
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
 	if err != nil {
 		return nil, err
 	}
@@ -17,7 +18,7 @@ func NewMongoConnect() (*mongo.Database, error) {
 		return nil, err
 	}
 
-	db := client.Database("global")
+	db := client.Database(os.Getenv("MONGODB_DATABASE"))
 
 	return db, err
 }
